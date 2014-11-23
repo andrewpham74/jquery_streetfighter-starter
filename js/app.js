e889bfcbef24bcc0093e033c0bc33f59103f3dfa
andrewpham74@gmail.com <tiff.t.pham@gmail.com>
2014-11-23 11:53:34 -0500

    Completed project

--- js/app.js
+++ js/app.js
@@ -7,15 +7,40 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
-.mousedown(function() {
-    // play hadouken sound
+ .mousedown(function() {
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // animate hadouken to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+
   })
   .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+
+  $(document).keydown(function(event) {
+    if(event.which === 88) {
+      $(".ryu-ready").hide();
+      $(".ryu-cool").show();
+
+      setTimeout(function() {
+        $(".ryu-cool").hide();
+        $(".ryu-ready").show();
+      }, 1000);
+    }
+  });
 });
-});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
